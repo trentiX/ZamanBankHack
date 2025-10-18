8349a18afa8cb35cd4a4dc288edbfffb5ab93f9f
Feruum <163900063+Feruum@users.noreply.github.com>
2025-10-18 23:46:44 +0500

    Add files via upload

---
+++ web/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
+const fs = require("fs");
+
+const app = express();
+const PORT = 8000;
+
+// Включаем gzip/brotli поддержку
+app.use(compression());
+
+// Правильная обработка .br файлов через middleware
+app.use((req, res, next) => {
+  if (req.url.endsWith(".br")) {
+    res.setHeader("Content-Encoding", "br");
+    res.setHeader("Content-Type", "application/javascript");
+  }
+  if (req.url.endsWith(".gz")) {
+    res.setHeader("Content-Encoding", "gzip");
+    res.setHeader("Content-Type", "application/javascript");
+  }
+  next();
+});
+
+// Раздаём всё содержимое папки как статику
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+  console.log(`✅ Сервер запущен: http://localhost:${PORT}`);
+});
